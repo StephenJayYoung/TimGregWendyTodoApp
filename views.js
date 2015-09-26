9a54cd97a1cc6787538f9ff9f06cedcd6a264060
Stephen Young <stephenjayyoung@gmail.com>
2015-09-26 16:48:50 -0700

    moved contents of folder/files for jse project into this one

---
+++ views.js
@@ -0,0 +1,135 @@
+var GUI = (function(){ //IIFE for all Views
+
+var IssueView = Backbone.View.extend({
+  render: function () {
+		var title = this.model.get("title");
+		var description = this.model.get("description");
+		var creator = this.model.get("creator");
+		var assignee = this.model.get("assignee");
+		var status = this.model.get("status");
+        this.$el.html("<div>Task:<br>" + title + " " + description + "</div>");
+    },
+});
+
+var CreateTaskView = Backbone.View.extend({
+
+});
+
+var UnassignedTasksView = Backbone.View.extend({
+	render: function () {
+		var issues = this.collection;
+		var self = this;
+		issues.forEach(function(issue){
+			var issueView = new IssueView({model: issue});
+			issueView.render();
+			self.$el.append(issueView.$el);
+		});
+	}
+});
+
+var UserTasksView = Backbone.View.extend({
+
+});
+
+var UserView = Backbone.View.extend({
+	events: {
+		'click #logoutButton' : 'logOutUser',
+		'click #addTaskButton' : 'addTask'
+	},
+
+	logOutUser: function() {
+		console.log("hey");
+	},
+
+	addTask: function() {
+		console.log('yo');
+	},
+
+	initialize: function() {
+		console.log(this.collection);
+		console.log(this.collection.findWhere({status: 'unassigned'}));
+		console.log('hello');
+		this.render();
+	},
+
+	render: function() {
+		var taskSelf = this;
+		var logoutButton = '<button id ="logoutButton">Logout</button>';
+		var addTaskButton = '<button id ="addTaskButton">Add Task</button>';
+		taskSelf.$el.html("<div>Logout:</div>" + logoutButton + addTaskButton);
+		console.log("Am I rendering?");
+		$("#app").append(this.$el);
+	}
+
+});
+
+
+///The login View///
+var LoginView = Backbone.View.extend ({
+	
+	events : {
+		'click #nameButton': 'nameOfFunction'
+
+	},
+
+	nameOfFunction: function() {
+		// console.log(($('#typedName').val()));
+		var certainUserName = ($('#typedName').val());
+		var userValid = this.collection.findWhere({username: certainUserName});
+		var certainUserPassword = ($('#typedPassword').val());
+
+		if(userValid.attributes.password == certainUserPassword) {
+			console.log("Success");
+			this.remove(); //this removes the loginView contents from the page
+			var userView = new UserView({collection: app.issues}); //this allows us to access the 
+			//userView. We had to create a new instance of it, and it has to access a collection in order to work (data).
+			////The data that it needs to access is in app.issues (which is located in your main.js file).
+			userView.render(); //this renders the userView and allows us to see it in the browser
+			}
+		else {
+			alert("Nope");
+		};
+
+
+	},
+	initialize: function() {
+		//This is just to see processes-- doesn't actually move project forward//
+		// console.log(this.collection);
+		// console.log(this.collection.findWhere({username:'Person1'}));
+		// var username=this.collection.findWhere({username:'Person1'});
+		// console.log(username.attributes.password);
+		// console.log($(this.el));
+	},
+	render: function () {
+		var userSelf = this;
+		
+		// var username = this.collection.findWhere({username:'Person1'}).attributes.username;
+		var inputUsername = '<input type="text" id="typedName" value=" input username  " />'
+		console.log($('#typedName'));
+		var inputPassword = '<input type="text" id="typedPassword" value=" input password " />'
+		var userButton = '<button id ="nameButton">Enter</button>';
+		userSelf.$el.html("<br><div id ='loginArea'><div>Login:<br>" + inputUsername + "</div>" +
+			 "<div>Password:<br>" + inputPassword + "</div>" + userButton + "</div>")
+	},
+
+
+
+});
+
+
+// generic ctor to represent interface:
+function GUI(users,issues,el) {
+	var unAssignedTasks = new UnassignedTasksView({collection: issues});
+	unAssignedTasks.render();
+	$(el).append(unAssignedTasks.$el);
+
+	var login = new LoginView({collection: users});
+	login.render();
+	$(el).append(login.$el);
+
+	// var userPage = new UserView({collection: issues});
+	// $(el).append(userPage.$el); 
+};
+
+return GUI;
+}())
